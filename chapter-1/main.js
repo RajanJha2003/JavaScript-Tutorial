fad79128dc11a8631d450f1c8ce15d5c2834e1c3
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-05-16 12:19:39 +0530

    chapter 1- variables

---
+++ chapter-1/main.js
@@ -0,0 +1,28 @@
+// For Making Notes into your code is called comments
+
+//* 1. var (old way)
+
+var title = "Let's Learn Javascript⚡";
+
+var title = "This is one more title💫"
+
+title = "Javascript🔥"
+console.log(title);
+
+// *2. let ( new & modern way)
+
+let isDone = "Yes✅";
+// re-declare
+// let isDone = "No❌"
+
+// re-assign
+isDone = "No🔥";
+console.log(isDone);
+
+// *3. Const ( new & modern way)
+
+const PI = 3.14;
+// const PI = 22/7
+
+// PI = 12039120;
+console.log(PI);
